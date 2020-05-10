9c260ae5ea7c041e80198a54c05dd21e5b775fba
Andrekuzwa <48141764+Andrekuzwa@users.noreply.github.com>
2020-05-11 01:22:51 +0200

    ignore

--- Project/SWDISK.py
+++ Project/SWDISK.py
@@ -15,11 +15,27 @@ class UberFinder:
         self.deliverers_quantity = deliverers_quantity
         self.gmaps = googlemaps.Client(key=api_key)
 
-
+        #dictionary of restaurants in form
+        #{id:{
+        #       'loc':(float,float)
+        #       'name': string
+        #       'client': client_id
+        #     }
+        # }
         self.restaurants = {}
-
+        # dictionary of deliverers in form
+        # {id:{
+        #       'loc':(float,float)
+        #       'orders': list
+        #       'travel_mode': string (driving/walking/bycicling)
+        #     }
+        # }
         self.deliverers = {}
-
+        #dictionary
+        # {id:{
+        #       'loc':(float,float)
+        #     }
+        # }
         self.clients = {}
 
         #parameters for gmaps api search
@@ -28,12 +44,19 @@ class UberFinder:
                                 'location': (51.1117,17.0602),
                                 'radius': 500
                              }
+        #travel modes of deliverers
+        self.travel_mode = ('walking','bicycling','driving')
+
+        #travel time matrixes
+        #D - DELIVERER , R - RESTAURANT, C - CLIENT
+        #2D numpy arrays of seconds of travel between DR(deliverer <--> restaurant)
         self.travel_time_DR = np.arange(self.deliverers_quantity*self.restaurants_quantity).reshape(
             self.deliverers_quantity,self.restaurants_quantity)
         self.travel_time_RC = np.arange(self.restaurants_quantity * self.restaurants_quantity).reshape(
             self.restaurants_quantity, self.restaurants_quantity)
         self.travel_time_DC = np.arange(self.deliverers_quantity*self.restaurants_quantity).reshape(
             self.deliverers_quantity,self.restaurants_quantity)
+
         self.generate_restaurants()
         self.generate_deliverers()
         self.generate_clients()
@@ -43,25 +66,27 @@ class UberFinder:
         self.count_travel_time_DC()
 
     def generate_restaurants(self):
+        #adds restaurants to restaurants dictionary according to search parameters
+        #now searching restaurants in 500 meters radius from pasaż grunwaldzki
         restaurants_data = self.gmaps.places(**self.search_params)
         for i in range(self.restaurants_quantity):
             self.restaurants[i] = {'name': restaurants_data['results'][i]['name'],
                                    'loc': ((restaurants_data['results'][i]['geometry']['location']['lat'],
-                                                                        restaurants_data['results'][i]['geometry']['location']['lng'])),
+                                            restaurants_data['results'][i]['geometry']['locat  ion']['lng'])),
                                    'client': None
                                    }
 
-            # self.restaurants[restaurants_data['results'][i]['name']] = ((restaurants_data['results'][i]['geometry']['location']['lat'],
-            #                                                             restaurants_data['results'][i]['geometry']['location']['lng']))
-
     def generate_deliverers(self):
-        # for i in range(self.deliverers_quantity):
-        #     self.deliverers.append( (random.uniform(51.1070, 51.1175),random.uniform(17.055, 17.065)) )
+        # adds deliverers to deliverers dictionary
+        # now locations of deliverers are randomly generated 'around' pasaż grunwaldzki in (more or less) 1,5km radius
         for i in range(self.deliverers_quantity):
             self.deliverers[i] = {'loc' : (random.uniform(51.1070, 51.1175),random.uniform(17.055, 17.065)),
+                                  'travel_mode':random.choice(self.travel_mode),
                                   'orders': []}
 
     def generate_clients(self):
+        #adds clients to dictionary
+        # now locations of clients are randomly generated 'around' pasaż grunwaldzki in (more or less) 1,5km radius
         for i in range(self.restaurants_quantity):
             self.clients[i] = {'loc': (random.uniform(51.1070, 51.1175), random.uniform(17.055, 17.065))}
 
@@ -70,27 +95,27 @@ class UberFinder:
              self.restaurants[key]['client'] = client_id
 
     def count_travel_time_DR(self):
+        #fills travel time matrix between deliverers and restaurants
         for deliverer in self.deliverers:
             for restaurant in self.restaurants:
                 distance = self.gmaps.distance_matrix(self.deliverers[deliverer]['loc'], self.restaurants[restaurant]['loc'],'walking')
                 self.travel_time_DR[deliverer][restaurant] = distance['rows'][0]['elements'][0]['duration']['value']
-        # for i in self.deliverers:
-        #     print(self.deliverers[i]['loc'])
+
 
     def count_travel_time_RC(self):
+        # fills travel time matrix between restaurants and clients
         for restaurant in self.restaurants:
             for client in self.clients:
                 distance = self.gmaps.distance_matrix(self.restaurants[restaurant]['loc'], self.clients[client]['loc'],'walking')
                 self.travel_time_RC[restaurant][client] = distance['rows'][0]['elements'][0]['duration']['value']
 
     def count_travel_time_DC(self):
+        # fills travel time matrix between deliverers and clients
         for deliverer in self.deliverers:
             for client in self.clients:
                 distance = self.gmaps.distance_matrix(self.deliverers[deliverer]['loc'], self.clients[client]['loc'],'walking')
                 self.travel_time_DC[deliverer][client] = distance['rows'][0]['elements'][0]['duration']['value']
-    # def assign_clients_restaurants(self):
-    #     for restaurant in self.restaurants:
-    #         restaurant[client] =
+
 
 def main():
     finder = UberFinder(api_key,5,4)
@@ -107,5 +132,6 @@ def main():
     print(finder.travel_time_RC)
     print('TRAVEL TIMES DELIVERER - CLIENT')
     print(finder.travel_time_DC)
+
 if __name__ == '__main__':
     main()
